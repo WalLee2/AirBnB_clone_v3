dd87eda38629e48e0f687c07dc2f02183a66c634
philipyoo <philipyoo10@gmail.com>
2017-04-19 22:06:39 +0000

    Edit db storage tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -13,7 +13,7 @@ TODO: Need to fix removal of data from database after each test is run.
 
 For now, skipping these tests. Make sure to edit `skipIf` call.
 """
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE', 'fs') != 'nope', "nuhuh")
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE', 'fs') != 'db', "db")
 class Test_DBStorage(unittest.TestCase):
     """
     Test the file storage class
@@ -21,62 +21,56 @@ class Test_DBStorage(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """create a session"""
-        # close previous connexion to same database
-        storage._DBStorage__session.close()
-        cls.store = DBStorage()
         test_args = {'updated_at': datetime(2017, 2, 12, 00, 31, 53, 331997),
                      'id': "0234",
                      'created_at': datetime(2017, 2, 12, 00, 31, 53, 331900),
                      'name': 'wifi'}
         cls.model = Amenity(**test_args)
-        cls.store.reload()
-        cls.test_len = 0
-    """
-    @classmethod
-    def tearDownClass(cls):
-        cls.store._DBStorage__session.rollback()
-        cls.store._DBStorage__session.close()
         storage.reload()
-    """
+        cls.test_len = 0
 
     def test_all(self):
-        output = self.store.all('Amenity')
+        output = storage.all('Amenity')
         self.assertEqual(len(output), self.test_len)
 
     def test_new(self):
         # note: we cannot assume order of test is order written
-        self.test_len = len(self.store.all())
-        # self.assertEqual(len(self.store.all()), self.test_len)
+        self.test_len = len(storage.all())
+        self.assertEqual(len(storage.all()), self.test_len)
         self.model.save()
-        #self.store.reload()
-        self.assertEqual(len(self.store.all()), self.test_len + 1)
+        self.assertEqual(len(storage.all()), self.test_len + 1)
         a = Amenity(name="thing")
         a.save()
-        #self.store.reload()
-        self.assertEqual(len(self.store.all()), self.test_len + 2)
+        self.assertEqual(len(storage.all()), self.test_len + 2)
 
-#        self.store.delete(a)
-#        self.store.save()
+        storage.delete(a)
+#        storage.delete(self.model)
+        storage.save()
 
     def test_save(self):
-        test_len = len(self.store.all())
+        test_len = len(storage.all())
         a = Amenity(name="another")
         a.save()
-        #self.store.reload()
-        self.assertEqual(len(self.store.all()), test_len + 1)
+        self.assertEqual(len(storage.all()), test_len + 1)
         b = State(name="california")
-        self.assertNotEqual(len(self.store.all()), test_len + 2)
+        self.assertNotEqual(len(storage.all()), test_len + 2)
         b.save()
-        #self.store.reload()
-        self.assertEqual(len(self.store.all()), test_len + 2)
+        self.assertEqual(len(storage.all()), test_len + 2)
+
+        storage.delete(a)
+        storage.delete(b)
+        storage.save()
 
     def test_reload(self):
         self.model.save()
         a = Amenity(name="different")
         a.save()
-        self.store.reload()
-        for value in self.store.all().values():
+        for value in storage.all().values():
             self.assertIsInstance(value.created_at, datetime)
+
+        storage.delete(self.model)
+        storage.delete(a)
+        storage.save()
 
 if __name__ == "__main__":
     unittest.main()
