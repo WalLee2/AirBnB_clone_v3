05b98d4cfe518d0305a9b0929a4cf19d086a3ec0
Walton Lee <mr.waltonlee@me.com>
2017-04-19 22:39:38 +0000

    Retreiving the amount of objects within each class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Returning a JSON status
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -12,3 +13,14 @@ def json_status():
     return the status OK
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def count_cls_obj():
+    """
+    return the number of class objects within each class
+    """
+    return jsonify({"amenities": storage.count('Amenity'),
+                    "cities": storage.count('City'),
+                    "places": storage.count('Place'),
+                    "reviews": storage.count('Review'),
+                    "users": storage.count('User')})
