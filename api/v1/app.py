42f3bfeb3b99cd5d8e5b23442f3ff710364f66c3
Walton Lee <mr.waltonlee@me.com>
2017-04-19 21:42:15 +0000

    running on local host port 5000 and tears it down after it's done

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,19 +2,19 @@
 """
 
 """
-import flask from Flask
-import storage from models
-import app_views from api.v1.views
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def app_teardown():
+def app_teardown(self):
     storage.close()
 
 if __name__ == "__main__":
-    HBNB_API_HOST = 0.0.0.0
-    HBNB_API_PORT = 5000
-    flask run --host=HBNB_API_HOST --port=HBNB_API_PORT
+    HBNB_API_HOST = "0.0.0.0"
+    HBNB_API_PORT = "5000"
+    app.run (host=HBNB_API_HOST, port=HBNB_API_PORT)
