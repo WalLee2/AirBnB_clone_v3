1d25641b2bb64b2dc46cac45866ee6f389bdcf23
philipyoo <philipyoo10@gmail.com>
2017-04-25 11:54:48 +0000

    Edit CORS setup

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,10 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/*":
+                            {
+                                "origins": getenv("HBNB_API_HOST", "0.0.0.0")
+                            }})
 
 
 @app.teardown_appcontext
