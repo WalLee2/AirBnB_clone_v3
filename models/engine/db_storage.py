c7c18eddb27767670ee615f8ffde915b10f52647
Walton Lee <mr.waltonlee@me.com>
2017-04-25 20:42:50 +0000

    updating comments and moving them to the correct location

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This is the db_storage module
+This is the db_storage module that has methods to manipulate data and stores it
 """
 from models.base_model import Base
 from sqlalchemy import create_engine
