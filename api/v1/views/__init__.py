f46cfd847bf3d67a48105741c62f0afd9c8b186d
Walton Lee <mr.waltonlee@me.com>
2017-04-25 20:05:00 +0000

    adding comments

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""
+Importing all necessary APIs
+"""
 from flask import Blueprint
-
-
 app_views = Blueprint('hbnb', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -9,5 +10,5 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-#from api.v1.views.places_amenities import *
+from api.v1.views.places_amenities import *
 from api.v1.views.places_reviews import *
