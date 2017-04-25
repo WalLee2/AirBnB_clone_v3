760046c99ff6b6a79e6530d0d9e43d8d404ef319
philipyoo <philipyoo10@gmail.com>
2017-04-25 07:34:57 +0000

    Edit main api file to get environment variables if exists, else use default values

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ Running flask on local host and on port 5000
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
@@ -18,11 +19,12 @@ def app_teardown(self):
     """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
-    HBNB_API_HOST = "0.0.0.0"
-    HBNB_API_PORT = "5000"
+    HBNB_API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    HBNB_API_PORT = getenv("HBNB_API_PORT", "5000")
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT)
