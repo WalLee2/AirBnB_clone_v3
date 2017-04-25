029be2159df293e0f492e6ac3acc0b3ce8be7742
Walton Lee <mr.waltonlee@me.com>
2017-04-25 20:35:39 +0000

    adding comments

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This is the db_storage module
+"""
 from models.base_model import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import (sessionmaker, scoped_session)
@@ -9,9 +12,6 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-"""
-This is the db_storage module
-"""
 
 
 class DBStorage:
