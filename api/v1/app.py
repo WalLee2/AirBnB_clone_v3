ef6ae1e71a4e9d2cf6489ff74d5b4a3319647192
Walton Lee <mr.waltonlee@me.com>
2017-04-26 04:34:43 +0000

    Simplifying CORS statement

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,12 +11,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-"""
-cors = CORS(app, resources={r"/api/*":
-                            {
-                                "origins": getenv("HBNB_API_HOST", "0.0.0.0")
-                            }})
-"""
+CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
